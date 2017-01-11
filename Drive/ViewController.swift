f3d4f3002876cca3e54fba6dd6a2b299c6c63abe
Marc Rollin <rollin.marc@gmail.com>
2017-01-11 10:31:24 +0100

    Intiate the rewrite * New Xcode 8.2.1 project for Swift 3.0.2

---
+++ Drive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drive
+//
+//  Created by Marc Rollin on 2017/01/11.
+//  Copyright © 2017 Safecast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
